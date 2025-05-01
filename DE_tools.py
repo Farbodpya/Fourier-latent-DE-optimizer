e3e262c681e07a326f284ac995166f0962fbbd06
Farbodpya <128262522+Farbodpya@users.noreply.github.com>
2025-05-01 12:26:13 +0330

    Update DE_tools.py

--- DE_tools.py
+++ DE_tools.py
@@ -1,20 +1,19 @@
 from scipy.optimize import differential_evolution
-from .fourier_projection import decode
+from fourier_projection import decode
 
-def run_de_in_latent_space(func, dim, latent_dim, bounds, maxiter=1000):
+def run_de_in_latent_space(func, dim, latent_dim, maxiter=1000):
+    bounds = [(-10, 10)] * latent_dim
     costs_per_generation = []
 
     def objective_1d(z):
         x = decode(z, dim).flatten()
         return func(x)
 
-    def callback_de(xk, convergence):
+    def callback(xk, convergence):
         cost = objective_1d(xk)
         costs_per_generation.append(cost)
 
-    result = differential_evolution(
-        objective_1d, bounds, strategy='best1bin',
-        maxiter=maxiter, callback=callback_de, disp=True
-    )
+    result = differential_evolution(objective_1d, bounds, strategy='best1bin',
+                                    maxiter=maxiter, callback=callback, disp=True)
 
-    return result, costs_per_generation
+    return result.fun, costs_per_generation
